f5f19954c20b96eba55f0caf12da43b104f05025
Matt Link <Linkm1993@gmail.com>
2020-05-27 22:45:29 -0400

    Heroku

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
+const path = require("path")
 
 router.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/index.html"), {title:"Fitness Tracker"});
